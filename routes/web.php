4165ba3996473f5a9d8a227825997617c1e4a569
Rohit <namdeo.madhi99@gmail.com>
2022-02-03 23:07:55 +0530

    first commit

---
+++ routes/web.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use Illuminate\Support\Facades\Mail;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    //return view('welcome');
+    $data = ['name'=>'Rohit Namdeo'];
+    Mail::send([], [], function($msg){
+        $msg->to('namdeo.madhi50@gmail.com', 'Rohit Namdeo')
+            ->subject('Testing mail')
+            ->setBody('Hi, this is working fine');
+    });
+
+    echo "mail sent";
+});
